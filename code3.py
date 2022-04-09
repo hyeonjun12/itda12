f1cece0e12fe92e3533e8814524fd23f5fde2992
hyeonjun12 <102644603+hyeonjun12@users.noreply.github.com>
2022-04-09 22:43:31 +0900

    Add files via upload

---
+++ code3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+a,b,c,d=map(int, input().split())
+if a>=90 and b>80 and c>85 and d>=80:
+    print("True")
+else:
+    print("False")
+
+
+# In[31]:
+
+
+s="""'Python' is a "programming language"
+that lets you work quickly
+and
+integrate systems more effectively."""
+print(s)
+
+
+# In[33]:
+
+
+b=int(input())
+a=tuple(range(-10,10,b))
+print(a)
+
+
+# In[ ]:
+
+
+
+
